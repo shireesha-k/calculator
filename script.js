a14884b9a39be146bf198cca5b9e15e830c49619
shireesha-k <katravaththrisha@gmail.com>
2024-10-24 19:13:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let string=" ";
+let buttons=document.querySelectorAll("button");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML==='='){
+            string=eval(string);
+            document.getElementById('display').value=string;
+        }
+        else if(e.target.innerHTML==='Delete'){
+            string="";
+            document.getElementById('delete').value=string;
+        }
+        else{
+            console.log(e.target);
+            string=string+ e.target.innerHTML;
+            document.getElementById('display').value=string;
+        }
+    });
+});
